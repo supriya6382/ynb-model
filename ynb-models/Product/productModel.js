ae06c3b079fd4a9d874b97a5835dc6a9958dfeea
supriya6382 <125237916+supriya6382@users.noreply.github.com>
2023-03-30 09:39:17 +0530

    Delete ynb-models directory

--- ynb-models/Product/productModel.js
+++
@@ -1,8 +0,0 @@
-const Mongoose = require('mongoose');
-
-const productDetails = {
-    productId: { type: String },
-    productName: { type: String },
-    categoryCode: { type: String },
-    imageUrl: { type: String }
-}
